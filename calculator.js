55dec2a3de75460acb9b05c7bfade195abdda177
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 15:08:58 +0000

    Initial setup of maths functions in console

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+console.log("1 + 2 = " + add(1,2));
+console.log("10 - 2 = " + subtract(10,2));
+console.log("5 * 2 = " + multiply(5,2));
+console.log("9 / 3 = " + divide(9,3));
