dd96059ccfe141b1f506b8743d205af0c9c5191b
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 15:17:43 +0000

    Added operate function to call the operators

--- calculator.js
+++ calculator.js
@@ -18,3 +18,26 @@ console.log("1 + 2 = " + add(1,2));
 console.log("10 - 2 = " + subtract(10,2));
 console.log("5 * 2 = " + multiply(5,2));
 console.log("9 / 3 = " + divide(9,3));
+
+function operate(numberOne,numberTwo,operator){
+    if(operator === "+")
+    {
+        return add(numberOne,numberTwo);
+    }
+    else if(operator === "-")
+    {
+        return subtract(numberOne,numberTwo);
+    }
+    else if(operator === "*")
+    {
+        return multiply(numberOne,numberTwo);
+    }
+    else if(operator === "/")
+    {
+        return divide(numberOne,numberTwo);
+    }
+}
+
+let numberOne;
+let numberTwo;
+let operator;
